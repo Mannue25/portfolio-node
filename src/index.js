4f776b4d7306276298b5f0092bb88668604b1cc9
mannue25 <manuelgaleana2590@gmail.com>
2021-01-11 13:32:45 -0600

    server fixed

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const router = require('./routes/index');
 
 // Settings
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000;
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
